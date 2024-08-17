dab910ddca166883de46f0cbaa6f654ab1d282a3
Magio-ore <74238314+Magio-ore@users.noreply.github.com>
2024-08-17 11:26:40 +0300

    Add files via upload

---
+++ Work2/main.py
@@ -0,0 +1,28 @@
+from pathlib import PurePath, Path
+
+path = PurePath("./cats_file.txt")
+
+def get_cats_info(path):
+    if Path(path).exists():  # Перевірка, чи існує файл
+        cats_info = [] 
+        with open(path, 'r', encoding="utf-8") as file:
+            for line in file:
+                # Розділяємо рядок по комі
+                parts = line.strip().split(',')
+                
+                # Перевіряємо, що рядок має три частини
+                if len(parts) == 3:
+                    cat_dict = {
+                        "id": parts[0].strip(),
+                        "name": parts[1].strip(),
+                        "age": parts[2].strip()
+                    }
+                    cats_info.append(cat_dict)
+                else:
+                    print(f"Рядок {parts} містить більше ніж 3 частини")
+        
+        print(cats_info)
+    else:
+        print("Файл не знайдено або він пошкоджений!")
+
+get_cats_info(path)
